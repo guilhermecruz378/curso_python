eb37a191089dd1529e76736555cf5588e3fb388b
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-07-07 20:36:38 -0300

    enumerate

--- aula52enumerate.py
+++ aula52enumerate.py
@@ -1,6 +1,19 @@
 lista = ['maria', 'joao','matias']
+lista.append('Neuza')
+
+#lista_enumerada = list(enumerate(lista))
+#print(lista_enumerada)
 
 lista_enumerada = enumerate(lista)
 
-for nomes in lista_enumerada:
-    print(nomes)
+# for item in lista_enumerada:
+#     indicie, valor = item
+#     print(indicie, valor)
+
+# for indicie, valor in lista_enumerada:# isso é como se fosse um for dentro de um for
+#     print(indicie, valor)
+
+for Tupla_enumerada in lista_enumerada:
+    print('For da Tupla: ')
+    for valor in Tupla_enumerada:
+            print(f'\t{valor}')
