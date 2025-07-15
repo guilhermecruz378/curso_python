340b73b44f4c22209054935e0ad713e0b8b2c35c
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-07-15 00:05:42 -0300

    listas

---
+++ aula56listasinlistas.py
@@ -0,0 +1,31 @@
+"""salas = [
+    ['maria', 'helena'],
+    ['elaine'],
+    ['jao','luiz', 'heitor']
+]
+
+for itens in salas:
+    for separados in enumerate(itens):
+        print(separados)"""
+
+"""
+Lista de listas e seus índices
+"""
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+# print(salas[1][0])
+# print(salas[0][1])
+# print(salas[2][2])
+# print(salas[2][3][3])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
