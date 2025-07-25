3dbd62895e88e2444c92f4c60b66fc5526862e72
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-07-25 16:32:53 -0300

    interpretador, desempacotamento, ternária

---
+++ aula59.py
@@ -0,0 +1,26 @@
+#Desempacotamento em chamadas 
+#De métodos e funções
+string = 'ABCD'
+lista = ['maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = ('python', 'é', 'legal')
+
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+# p, b, *_, antepenultimo, ultimo = lista
+# print(p,ultimo, antepenultimo)
+
+# print('maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, end='\n')
+print('-='*10)
+print(*salas, sep='\n')
