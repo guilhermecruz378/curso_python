6278b695012dd791afe29fd13511914f89686e78
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-07-29 21:04:30 -0300

    cpf

--- aula61CPF.py
+++ aula61CPF.py
@@ -1,26 +1,51 @@
 #CPF: 746.824.890-70
 
-cpf = '746.824.890'
+# cpf = '746.824.890'
+# cpf_sem_ponto = cpf.split('.')
+# cpf_junto = ''.join(cpf_sem_ponto)
 
-cpf_sem_ponto = cpf.split('.')
+# soma = 0
+# contador = 10
+# for num in cpf_junto:
+#     soma += int(num) * contador
+#     contador -=1
 
-cpf_junto = ''.join(cpf_sem_ponto)
+# total = soma * 10
+# if total % 11 < 9:
+#     digito = total % 11
+#     print('O primeiro digito é ', digito)
+# elif total % 11 > 9:
+#     digito = 0
+#     print('O primeiro digito é ', digito)
+# else:
+#     print('Erro')
 
-cpf_numerico = int(cpf_junto)
+cpf_enviado_pelo_usuario = '74682489070'
+nove_digitos = cpf_enviado_pelo_usuario[:9]
+contador_regressivo_1 = 10
 
-soma = 0
-contador = 10
-for num in cpf_junto:
-    soma += int(num) * contador
-    contador -=1
+resultado_digito1 = 0
+for digito in nove_digitos:
+    resultado_digito1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito1 * 10) % 11
+digito_1 = digito_1 if digito_1 < 9 else 0
+print(digito_1)
 
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
 
-total = soma * 10
-if total % 11 < 9:
-    digito = total % 11
-    print('O primeiro digito é ', digito)
-elif total % 11 > 9:
-    digito = 0
-    print('O primeiro digito é ', digito)
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 < 9 else 0
+print(digito_2)
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_pelo_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_pelo_usuario} é válido')
 else:
-    print('Erro')
+    print('cpf inválido!')
